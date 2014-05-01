0de44092c5ab6ba6d38871f0e48a78da78786f3a
twflu <yzzhou@thoughtworks.com>
2014-05-01 12:36:44 +0800

    [#N/A][yzzhou] - add jasmine reporter.

--- karma.conf.js
+++ karma.conf.js
@@ -14,6 +14,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+        'lib/*.js',
         'js/*.js',
         'test/*.js'
     ],
--- test/personSpec.js
+++ test/personSpec.js
@@ -80,3 +80,28 @@ describe('person test', function(){
 
     });
 });
+
+(function(){
+    var jasmineEnv = jasmine.getEnv();
+    jasmineEnv.updateInterval = 250;
+
+    var htmlReporter = new jasmine.HtmlReporter();
+    jasmineEnv.addReporter(htmlReporter);
+
+    jasmineEnv.specFilter = function(spec){
+        return htmlReporter.specFilter(spec);
+    };
+    var currentWindowOnload = window.onload;
+    window.onload = function () {
+        if (currentWindowOnload) {
+            currentWindowOnload();
+        }
+//        document.querySelector('.version').innerHTML = jasmineEnv.versionString();
+        execJasmine();
+    };
+
+    function execJasmine() {
+        jasmineEnv.execute();
+    }
+
+})();
